38caf2c21581718d8d605fadc2a6b315e50648d7
emert117 <emremert117@gmail.com>
2022-12-29 14:47:28 +0300

    added 2D_Array_DS

---
+++ Arrays/2D_Array_DS/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'hourglassSum' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int hourglassSum(List<List<int>> arr)
+    {
+        int maxSum = int.MinValue;
+        // traverse each hour glass
+        for (int row = 0; row < 4; row++)
+        {
+            for (int column = 0; column < 4; column++)
+            {
+                int sum = arr[row][column] + arr[row][column+1] + arr[row][column+2];
+                sum += arr[row+1][column+1];
+                sum += arr[row+2][column] + arr[row+2][column+1] + arr[row+2][column+2];
+
+                if (sum > maxSum)
+                    maxSum = sum;
+            }                    
+        }
+
+        return maxSum;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        List<List<int>> arr = new List<List<int>>();
+
+        for (int i = 0; i < 6; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+        }
+
+        int result = Result.hourglassSum(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
