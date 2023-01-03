68c9feeb7011962f7bb24df9c06f550241b2aec7
emert117 <emremert117@gmail.com>
2023-01-03 14:24:57 +0300

    added MinimumSwaps2

---
+++ Arrays/MiniumSwap2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the minimumSwaps function below.
+    static int minimumSwaps(int[] arr) {
+        int swaps = 0;
+        for (int i=1; i<arr.Length; i++)
+        {
+            // find i
+            // if it is not in the right place swap it with its right place
+            if (i != arr[i-1])
+            {
+                int index = Array.IndexOf(arr, i);
+                int temp = arr[i-1];                
+                arr[i-1] = i;
+                arr[index] = temp;
+                swaps++;
+            }
+            
+        }
+        return swaps;
+    }    
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int m = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<List<int>> queries = new List<List<int>>();
+
+        for (int i = 0; i < m; i++)
+        {
+            queries.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(queriesTemp => Convert.ToInt32(queriesTemp)).ToList());
+        }
+
+        long result = Result.arrayManipulation(n, queries);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
