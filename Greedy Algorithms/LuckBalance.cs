d0ab67087f77c483bd5359bbea96f8cbe26ff17c
Emre MERT <emremert117@gmail.com>
2023-01-22 19:36:52 +0300

    added Luck Balance

---
+++ Greedy Algorithms/LuckBalance.cs
@@ -0,0 +1,83 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'luckBalance' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. 2D_INTEGER_ARRAY contests
+     */
+
+    public static int luckBalance(int k, List<List<int>> contests)
+    {
+        int totalLuck = 0;
+        PriorityQueue<int, int> pq= new ();
+        foreach(var contest in contests) {
+            int luck = contest[0];
+            int importance = contest[1];
+            if (importance == 0) {
+                totalLuck += luck;
+            }
+            else {
+                if(pq.Count < k) {
+                    pq.Enqueue(luck, luck);
+                }
+                else {
+                    int lowest = pq.EnqueueDequeue(luck, luck);
+                    totalLuck -= lowest;
+                }
+            }
+        }
+        
+        while(pq.Count > 0) {
+            totalLuck += pq.Dequeue();
+        }
+               
+        return totalLuck;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<List<int>> contests = new List<List<int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            contests.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(contestsTemp => Convert.ToInt32(contestsTemp)).ToList());
+        }
+
+        int result = Result.luckBalance(k, contests);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
