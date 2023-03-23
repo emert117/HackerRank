4a1be2cd9694baccd282edac8bc6351279662616
Emre MERT <emremert117@gmail.com>
2023-03-23 13:09:21 +0300

    added Breaking the Records

---
+++ Implementation/BreakingTheRecords.cs
@@ -0,0 +1,67 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'breakingRecords' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY scores as parameter.
+     */
+
+    public static List<int> breakingRecords(List<int> scores)
+    {
+        int minRecords = 0, maxRecords = 0;
+        int min = scores[0], max = scores[0];
+        
+        foreach(int score in scores)
+        {
+            if (score < min)
+            {
+                min = score;
+                minRecords++;                
+            }
+            
+            if (score > max)
+            {
+                max = score;
+                maxRecords++;                
+            }
+        }
+        //Console.WriteLine($"{minRecords} {maxRecords}");
+        return new List<int>() {maxRecords, minRecords};
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> scores = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(scoresTemp => Convert.ToInt32(scoresTemp)).ToList();
+
+        List<int> result = Result.breakingRecords(scores);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
